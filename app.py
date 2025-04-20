fd413ab91c078e825f642228a29c7064ff186020
ebrahim <ebrahimnobiqe@gmail.com>
2025-04-20 14:47:44 +0330

    i start to write flask app web server

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
 from flask import Flask, request, jsonify, send_from_directory, render_template, redirect, url_for
+
+
+
+
+
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')  # Render index.html
+
+
+
+
+
+
+# Run the Flask app
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
