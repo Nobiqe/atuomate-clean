6bb76b82e274765ced91ef310ff1d5c81f81ba1b
ebrahim <ebrahimnobiqe@gmail.com>
2025-04-21 16:34:51 +0330

    added my libraries

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
+# Import necessary libraries for file handling, Flask, threading, scheduling, and utilities
+import os
+import random
+import shutil
+import traceback
+from datetime import datetime, timedelta
 from flask import Flask, request, jsonify, send_from_directory, render_template, redirect, url_for
+from werkzeug.utils import secure_filename
+from concurrent.futures import ThreadPoolExecutor
+from apscheduler.schedulers.background import BackgroundScheduler
+from id_card_processor import process_id_card
+from functools import wraps
+import magic  # For validating file MIME types
 
-
-
-
-
-
+app = Flask(__name__, static_folder='output_images', static_url_path='/output_images')
 
 @app.route('/')
 def home():
     return render_template('index.html')  # Render index.html
+
+
+@app.route('/api/upload',methods=['POST'])
 
 
 
